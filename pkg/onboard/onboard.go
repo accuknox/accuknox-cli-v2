594af75a138e0fb7baae8bb501f8144acd8fef1e
Eswar Rajan Subramanian <eswar@accuknox.com>
2026-07-28 12:07:03 +0530

    Remove custom tag for SPIRE

--- pkg/onboard/onboard.go
+++ pkg/onboard/onboard.go
@@ -427,7 +427,7 @@ func (cc *ClusterConfig) PopulateImageDetails(releaseInfo cm.ReleaseMetadata, im
 
 		cc.SPIREAgentImage, err = getImage(VMMode_Docker, registry, cm.DefaultDockerRegistry,
 			cm.DefaultAccuKnoxRepo, images.SpireImage, cm.DefaultSPIREAgentImage,
-			"latest", releaseInfo.SPIREAgentImageTag, "", "", preserveUpstream)
+			"", releaseInfo.SPIREAgentImageTag, "", "", preserveUpstream)
 		if err != nil {
 			return err
 		}
